6606582ca083329079a40db282a618e0c88c5bb5
Alexandr Kalevich <alexandr.kalevich@gmail.com>
2022-05-30 14:15:59 +0300

    28 Service

---
+++ app/src/test/java/com/github/krottv/tmstemp/ExampleUnitTest.kt
@@ -0,0 +1,23 @@
+package com.github.krottv.tmstemp
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        var a = 1
+        var b : Int = a
+        println(a)
+        println(b)
+        a = 2
+        println(a)
+        println(b)
+    }
+}
